25ffc0a79cd18bb40594a2a442b3925bbda9d633
guozhaoxuan <1175398345@qq.com>
2025-07-12 14:10:30 +0800

    feat:sql

--- src/migrations/sequence_number.sql
+++ src/migrations/sequence_number.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `sequence_number` (
-   `key` varchar(50) NOT NULL DEFAULT '',
-   `counter` int unsigned NOT NULL DEFAULT '0',
+   `key` varchar(50) NOT NULL DEFAULT '' COMMENT 'key值',
+   `counter` int unsigned NOT NULL DEFAULT '0' COMMENT '计次',
    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
    PRIMARY KEY (`key`) USING BTREE
